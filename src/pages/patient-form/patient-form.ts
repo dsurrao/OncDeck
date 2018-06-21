6ad5cce959c868f384cfdfd9f19ada9d830c597c
Dominic Surrao <dsurrao@hotmail.com>
2018-06-20 21:18:36 -0400

    patient form: Update

--- src/pages/patient-form/patient-form.ts
+++ src/pages/patient-form/patient-form.ts
@@ -22,10 +22,11 @@ AWS.config.region = aws_exports.aws_project_region;
   templateUrl: 'patient-form.html',
 })
 export class PatientFormPage {
-  userId: string;
+  patientId: string;
   firstName: string;
   lastName: string;
   biopsyStatus: string;
+  patient: any;
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
@@ -34,35 +35,70 @@ export class PatientFormPage {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad PatientFormPage');
+    this.patient = this.navParams.data.params;
+    if (this.patient != null) {
+      this.patientId = this.patient['Id'];
+      this.firstName = this.patient['FirstName'];
+      this.lastName = this.patient['LastName'];
+      this.biopsyStatus = this.patient['BiopsyStatus'];
+    }
+    else {
+      this.patientId = UUID.v4();
+    }
   }
 
   submit() {
     Auth.currentUserCredentials()
       .then(credentials => {
-        this.userId = credentials.identityId;
+        if (this.patient == null) {
+          const params = {
+            TableName: 'Patient',
+            Item: {
+              LastName: this.lastName,
+              FirstName: this.firstName,
+              BiopsyStatus: this.biopsyStatus,
+              Id: this.patientId
+            }
+          };
 
-        const params = {
-          TableName: 'Patient',
-          Item: {
-            LastName: this.lastName,
-            FirstName: this.firstName,
-            BiopsyStatus: this.biopsyStatus,
-            Id: UUID.v4()
-          }
-        };
+          this.db.getDocumentClient(credentials).put(params).promise()
+            .then(data => { 
+              console.log(data);
+              this.events.publish('patientSaved');
+              this.navCtrl.pop();
+            })
+            .catch(err => console.log('error in save patient', err));
+        }
+        else {
+          const params = {
+            TableName: 'Patient',
+            Key: {
+              Id: this.patientId
+            },
+            ExpressionAttributeNames: {
+              '#l': 'LastName',
+              '#f': 'FirstName',
+              '#b': 'BiopsyStatus'
+             }, 
+            ExpressionAttributeValues: {
+              ':l': this.lastName,
+              ':f': this.firstName,
+              ':b': this.biopsyStatus
+            }, 
+            UpdateExpression: 'SET #l = :l, #f = :f, #b = :b'
+          };
 
-        this.db.getDocumentClient(credentials).put(params).promise()
-          .then(data => { 
-            console.log(data);
-            this.events.publish('patientSaved');
-            this.navCtrl.pop();
-          })
-          .catch(err => console.log('error in save patient', err));
+          this.db.getDocumentClient(credentials).update(params).promise()
+            .then(data => { 
+              console.log(data);
+              this.events.publish('patientSaved');
+              this.navCtrl.pop();
+            })
+            .catch(err => console.log('error in save patient', err));
+        }
       })
       .catch(err => {
         console.log('get current credentials err', err);
       });
   }
-
 }
--- src/pages/patient/patient.ts
+++ src/pages/patient/patient.ts
@@ -1,8 +1,18 @@
+import { BiopsyReport } from './../../models/biopsy-report';
+import { DynamodbProvider } from './../../providers/dynamodb/dynamodb';
+import { PatientFormPage } from './../patient-form/patient-form';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+import { Events, IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 import { Patient } from '../../models/patient';
 import { LoginModal } from '../../modal/login/login';
 import { LogoutModal } from '../../modal/logout/logout';
+
+import { Auth } from 'aws-amplify';
+import aws_exports from '../../assets/aws-exports'; 
+import AWS from 'aws-sdk';
+import { GetItemInput } from 'aws-sdk/clients/dynamodb';
+
+AWS.config.region = aws_exports.aws_project_region;
 
 /**
  * Generated class for the PatientPage page.
@@ -21,12 +31,38 @@ export class PatientPage {
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
-    public modalCtrl: ModalController) {
-    // require login
+    public modalCtrl: ModalController,
+    public db: DynamodbProvider,
+    public events: Events) {
     this.patient = this.navParams.data.params;
+    this.events.subscribe('patientSaved', () => {
+      this.refresh();
+    });
   }
 
-  addPatient() {
+  edit() {
+    this.navCtrl.push(PatientFormPage, {params: this.patient});
   }
 
+  refresh() {
+    Auth.currentUserCredentials()
+      .then(credentials => {
+
+        const params: GetItemInput = {
+          TableName: 'Patient',
+          Key: {
+            Id: this.patient['Id']
+          }
+        };
+
+        this.db.getDocumentClient(credentials).get(params).promise()
+          .then(data => { 
+            this.patient = data.Item;
+          })
+          .catch(err => console.log('error in save patient', err));
+      })
+      .catch(err => {
+        console.log('get current credentials err', err);
+      });
+  }
 }
