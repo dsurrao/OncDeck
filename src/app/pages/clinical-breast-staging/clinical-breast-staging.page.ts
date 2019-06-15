19707a728057279dacca5333010ae2dfc56ea4fa
Ben Ho <benjamin.g.ho@gmail.com>
2019-06-15 13:24:15 -0400

    added more radio buttons and descriptions

--- src/app/pages/clinical-breast-staging/clinical-breast-staging.page.ts
+++ src/app/pages/clinical-breast-staging/clinical-breast-staging.page.ts
@@ -8,13 +8,73 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ClinicalBreastStagingPage implements OnInit {
 
-  constructor(public route: ActivatedRoute) {}
+  isHideSubRadioButtons: boolean;
+  isHideDescription: boolean;
+  clinicalOptionText: string;
 
-  ngOnInit() {
-
-    let patientId = this.route.snapshot.paramMap.get('patientId');
-    console.log(patientId);
-
+  constructor(public route: ActivatedRoute) {
   }
 
+  ngOnInit() {
+    let patientId = this.route.snapshot.paramMap.get('patientId');
+    console.log(patientId);
+    this.isHideSubRadioButtons = true;
+    this.isHideDescription = true;
+  }
+
+  clickClinicalTumorStage(event) {
+    this.isHideDescription = false;
+    console.log("value chosen: " + event.detail.value);
+    if ((event.detail.value == 'T1')
+      || (event.detail.value == 'T1mi')
+      || (event.detail.value == 'T1a')
+      || (event.detail.value == 'T1b')
+      || (event.detail.value == 'T1c')
+    )
+    {
+      this.isHideSubRadioButtons = false;
+    }
+    else
+      this.isHideSubRadioButtons = true;
+
+    if (event.detail.value == 'TX') {
+      this.clinicalOptionText = "TX - Primary tumor cannot be assessed";
+    }
+
+    if (event.detail.value == 'T0') {
+      this.clinicalOptionText = "T0 - No evidence of primary tumor";
+    }
+
+    if (event.detail.value == 'Tis(DCIS)') {
+      this.clinicalOptionText = "Tis(DCIS) - Ductal carcinoma in situ";
+    }
+
+    if (event.detail.value == 'Tis(Paget)') {
+      this.clinicalOptionText = "Tis (Paget) - Paget's disease of the nipple NOT associated with invasive carcinoma and/or carcinoma in situ (DCIS) in the underlying breast parenchyma.";
+    }
+
+    if (event.detail.value == 'T1') {
+      this.clinicalOptionText = "T1 - Tumor < or = 20 mm in greatest dimension";
+    }
+
+    if (event.detail.value == 'T1mi') {
+      this.clinicalOptionText = "T1mi - Tumor < or = 1 mm in greatest dimension";
+    }
+
+    if (event.detail.value == 'T1a') {
+      this.clinicalOptionText = "T1a - Tumor > 1 mm but < or = 5 mm in greatest dimension (round any measurement > 1.0-1.9 mm to 2 mm)";
+    }
+
+    if (event.detail.value == 'T1b') {
+      this.clinicalOptionText = "T1b - Tumor > 5 mm but < or = 10 mm in greatest dimension";
+    }
+
+    if (event.detail.value == 'T1c') {
+      this.clinicalOptionText = "T1c - Tumor > 10 mm but < or = 20 mm in greatest dimension";
+    }
+
+    if (event.detail.value == 'T2') {
+      this.clinicalOptionText = "T2 - Tumor > 20 mm but < or = 50 mm in greatest dimension";
+    }
+  }
 }
