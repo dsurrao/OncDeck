6274d673096544f315d86ec3fbab36a42dbbb143
Ben Ho <benjamin.g.ho@gmail.com>
2019-07-17 23:51:41 -0400

    Created 'clinical-breast-staging-component-items'

---
+++ src/app/pages/patient/clinical-breast-staging-items/clinical-breast-staging-items.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Patient } from 'src/app/models/patient';
+
+@Component({
+  selector: 'clinical-breast-staging-items',
+  templateUrl: './clinical-breast-staging-items.component.html',
+  styleUrls: ['./clinical-breast-staging-items.component.scss'],
+})
+export class ClinicalBreastStagingItemsComponent implements OnInit {
+
+  @Input('patient') patient: Patient;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/pages/patient/patient.module.ts
+++ src/app/pages/patient/patient.module.ts
@@ -9,6 +9,7 @@ import { PatientPage } from './patient.page';
 import { PatientSummaryComponent } from 'src/app/components/patient-summary/patient-summary.component';
 import { RadiationTherapyItemsComponent } from './radiation-therapy-items/radiation-therapy-items.component';
 import { CompletedBiopsyItemsComponent } from './completed-biopsy-items/completed-biopsy-items.component';
+import { ClinicalBreastStagingItemsComponent } from './clinical-breast-staging-items/clinical-breast-staging-items.component';
 import { BiopsyPageModule } from '../biopsy/biopsy.module';
 import { RadiationTherapyPageModule } from '../radiation-therapy/radiation-therapy.module';
 import { SurgeryPageModule } from '../surgery/surgery.module';
@@ -32,6 +33,7 @@ import { TimelineComponent } from './timeline/timeline.component';
     PatientSummaryComponent,
     RadiationTherapyItemsComponent,
     CompletedBiopsyItemsComponent,
+    ClinicalBreastStagingItemsComponent,
     TimelineComponent
   ]
 })
