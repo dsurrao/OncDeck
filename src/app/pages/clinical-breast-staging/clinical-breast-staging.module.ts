59dc34ef6db2103207634e1774244210fbd8fed6
Ben Ho <benjamin.g.ho@gmail.com>
2019-07-26 09:34:35 -0400

    Adding staging summary

--- src/app/pages/clinical-breast-staging/clinical-breast-staging.module.ts
+++ src/app/pages/clinical-breast-staging/clinical-breast-staging.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { ClinicalBreastStagingPage } from './clinical-breast-staging.page';
+import { StagingSummaryComponent } from './staging-summary/staging-summary.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,9 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [ClinicalBreastStagingPage]
+  declarations: [
+    ClinicalBreastStagingPage,
+    StagingSummaryComponent
+  ]
 })
 export class ClinicalBreastStagingPageModule {}
